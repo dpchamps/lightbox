1a3a35541cdb065176f197644800e8f87005ff2d
gasolinewaltz <gasolinewaltz@gmail.com>
2016-01-12 15:19:38 -0800

    translation framework implemented
    The transition module allows for chainable function calls. i.e. lightbox.translate(image).slideRight().fadeOut().replace(newImage).fadeIn().start()
    
    A cleaner api for transition manipulation.

--- src/modules/translate.js
+++ src/modules/translate.js
@@ -6,8 +6,54 @@ all of these functions are chainable
 "use strict";
 
 var translate = function(image){
+  var
+      timedFunctions = [],
+      complete,
+      done = new Promise(function(res){
+        complete = res;
+      });
+
+  function wait(t, fn){
+    return new Promise(function(res){
+      setTimeout(function(){
+        res(fn);
+      }, t);
+    });
+  }
+  function waterfall(){
+      if(timedFunctions.length > 0){
+        var timedFn = timedFunctions.shift();
+        return wait(timedFn.t, timedFn.fn).then(function(fn){
+          fn();
+          waterfall();
+        });
+      }else{
+        complete();
+      }
+  }
+  function stack(t,fn){
+    var timedFn = {t:t, fn:fn};
+    timedFunctions.push(timedFn);
+  }
   return{
-    image: image
+    image : image,
+    start: function(){
+      waterfall();
+      return done;
+    },
+    
+    test1: function(){
+      stack(1500, function(){
+        console.log("test1");
+      });
+      return this;
+    },
+    test2: function(){
+      stack(600, function(){
+        console.log("test2");
+      });
+      return this;
+    }
   };
 };
 
